448afb89803ba1fe62b570d5961506b1ffa01284
Maxim Shabanov <99maxwolf99@gmail.com>
2024-01-24 18:01:57 +0300

    fix curl

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-require "settings.php";
+// require "settings.php";
 require "ozonApi.php";
 require "db.php";
-require "../func.php";
+require "func.php";
 
-$ozon = new OzonApi(CLIENT_ID, API_KEY);
+$ozon = new OzonApi("626516", "ca136e0a-de7b-4512-a15f-b763e682d9dd");
 
 $getOrders = $ozon->getOrder();
 
--- ozonApi.php
+++ ozonApi.php
@@ -19,7 +19,7 @@ class OzonApi{
         "dir"=> "ASC",
         "filter"=> array(
                     "delivering_date_from"=> "2023-08-24T14:15:22Z",
-                    "delivering_date_to"=> "2023-08-T14:15:22Z",
+                    "delivering_date_to"=> "2023-08-25T14:15:22Z",
                     "delivery_method_id"=> [],
                     "provider_id"=> [],
                     "status"=> "awaiting_packaging",
@@ -40,13 +40,13 @@ class OzonApi{
     }
 
     protected function curl($method, $arParams){
-        $ch = curl_init("https://api-seller.ozon.ru".$method."?".json_encode($arParams));
+        $ch = curl_init("https://api-seller.ozon.ru".$method);
         curl_setopt_array($ch, array(
             CURLOPT_HEADER => false,
             CURLOPT_RETURNTRANSFER => false,
             CURLOPT_POST => true,
             CURLOPT_HTTPHEADER => array("Content-Type: application/json", "Client-Id: ".$this->client_id, "Api-Key: ".$this->api_key),
-            // CURLOPT_POSTFIELDS => http_build_query($arParams)
+            CURLOPT_POSTFIELDS => json_encode($arParams)
         ));
 
         $get = curl_exec($ch);
